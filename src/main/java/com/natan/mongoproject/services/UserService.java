dd8d5b49940f0cfb8b8e14f8d5369f26b5897601
Natan Ventura <natanvent@gmail.com>
2020-08-03 11:32:50 -0300

    Update

--- src/main/java/com/natan/mongoproject/services/UserService.java
+++ src/main/java/com/natan/mongoproject/services/UserService.java
@@ -38,4 +38,15 @@ public class UserService {
 		findById(id);
 		repo.deleteById(id);
 	}
+	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repo.save(newUser);
+	}
+
+	private void updateData(User newUser, User user) {
+		newUser.setEmail(user.getEmail());
+		newUser.setName(user.getName());
+	}
 }
