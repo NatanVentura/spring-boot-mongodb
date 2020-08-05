e8ff7630636c6d8e1316d715a35239b35b498169
Natan Ventura <natanvent@gmail.com>
2020-08-05 10:35:58 -0300

    AuthorDTO

--- src/main/java/com/natan/mongoproject/domain/Post.java
+++ src/main/java/com/natan/mongoproject/domain/Post.java
@@ -3,6 +3,8 @@ package com.natan.mongoproject.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.natan.mongoproject.dto.AuthorDTO;
+
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -10,18 +12,18 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post(){
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.setUser(author);
+		this.setAuthorDTO(author);
 	}
 
 	public String getId() {
@@ -56,11 +58,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 	
-	public User getUser() {
+	public AuthorDTO getAuthorDTO() {
 		return author;
 	}
 
-	public void setUser(User author) {
+	public void setAuthorDTO(AuthorDTO author) {
 		this.author = author;
 	}
 
