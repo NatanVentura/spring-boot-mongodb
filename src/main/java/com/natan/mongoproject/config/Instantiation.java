e54e718be7fe42c92184431a6f543d685c85eaa6
Natan Ventura <natanvent@gmail.com>
2020-08-05 10:43:26 -0300

    User posts

--- src/main/java/com/natan/mongoproject/config/Instantiation.java
+++ src/main/java/com/natan/mongoproject/config/Instantiation.java
@@ -40,10 +40,12 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("03/08/2020"),"Agua coca latao", "Mery mery pra gringo é mais caro",new AuthorDTO(bob));
 		Post post2 = new Post(null, sdf.parse("02/08/2020"),"Sensata", "Essa Manu pisa nesses machos escrotos",new AuthorDTO(maria));
-				
-				
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().add(post2);
+		bob.getPosts().add(post1);
+		userRepository.saveAll(Arrays.asList(maria,bob));
 		
 	}
 	
